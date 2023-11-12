23948d09bc096f2a36b18aa403bfb82ebde20f02
Hau Truong <truongthianhhau2422002@gmail.com>
2023-11-12 23:20:01 +0700

    soure code seach

---
+++ Model/db-connect.php
@@ -0,0 +1,7 @@
+<?php
+$host="localhost";
+$username="root";
+$password="";
+$database="recruitment_system";
+$conn=mysqli_connect($host,$username,$password,$database);
+mysqli_query($conn,"SET NAMES 'utf8'");
